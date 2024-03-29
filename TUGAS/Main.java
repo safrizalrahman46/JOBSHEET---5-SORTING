ed586fe140a53994a72a025763edb0a79eab4d9c
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-03-29 10:01:05 +0700

    PUSH2

--- TUGAS/Main.java
+++ TUGAS/Main.java
@@ -2,11 +2,45 @@ package TUGAS;
 
 public class Main {
     public static void main(String[] args) {
-        TravelPlatform platform = new TravelPlatform();
+        // Inisialisasi platform dengan jumlah maksimum penginapan
+        TravelPlatform platform = new TravelPlatform(10); // misalnya, array dari 10 elemen
+
         // Tambahkan penginapan ke platform
-        // platform.tambah(new Penginapan("Nama Penginapan", harga, rating));
+        platform.tambah(new Penginapan("Penginapan A", 100, 4));
+        platform.tambah(new Penginapan("Penginapan B", 80, 3));
+        platform.tambah(new Penginapan("Penginapan C", 120, 5));
+
+              // Output sebelum pengurutan
+              System.out.println("Sebelum diurutkan:");
+              for (Penginapan p : platform.daftarPenginapan) {
+                  if (p != null) {
+                      System.out.println("Nama: " + p.nama + ", Harga: " + p.harga + ", Rating: " + p.rating);
+                  }
+                 
+              }
+              System.out.println("===============================================================");
         // Urutkan dan tampilkan hasilnya
         platform.bubbleSortByHarga();
         platform.selectionSortByRating();
+        
+        // Tampilkan hasil setelah diurutkan
+        System.out.println("Sebelum diurutkan:");
+        for (Penginapan p : platform.daftarPenginapan) {
+            if (p != null) {
+                System.out.println("Nama: " + p.nama + ", Harga: " + p.harga + ", Rating: " + p.rating);
+            }
+           
+        }
+        System.out.println("===============================================================");
     }
 }
+// public class Main {
+//     public static void main(String[] args) {
+//         TravelPlatform platform = new TravelPlatform(10);
+//         // Tambahkan penginapan ke platform
+//         // platform.tambah(new Penginapan("Nama Penginapan", harga, rating));
+//         // Urutkan dan tampilkan hasilnya
+//         platform.bubbleSortByHarga();
+//         platform.selectionSortByRating();
+//     }
+// }
--- TUGAS/TravelPlatform.java
+++ TUGAS/TravelPlatform.java
@@ -4,6 +4,12 @@ public class TravelPlatform {
     Penginapan[] daftarPenginapan;
     int idx;
 
+    // Constructor untuk menginisialisasi array daftarPenginapan
+    TravelPlatform(int jumlahPenginapan) {
+        daftarPenginapan = new Penginapan[jumlahPenginapan];
+        idx = 0;
+    }
+
     // Method untuk menambah penginapan ke daftar
     void tambah(Penginapan p) {
         if (idx < daftarPenginapan.length) {
@@ -18,7 +24,8 @@ public class TravelPlatform {
     void bubbleSortByHarga() {
         for (int i = 0; i < daftarPenginapan.length - 1; i++) {
             for (int j = 0; j < daftarPenginapan.length - 1 - i; j++) {
-                if (daftarPenginapan[j].harga > daftarPenginapan[j + 1].harga) {
+                if (daftarPenginapan[j] != null && daftarPenginapan[j + 1] != null && 
+                    daftarPenginapan[j].harga > daftarPenginapan[j + 1].harga) {
                     Penginapan temp = daftarPenginapan[j];
                     daftarPenginapan[j] = daftarPenginapan[j + 1];
                     daftarPenginapan[j + 1] = temp;
@@ -26,19 +33,48 @@ public class TravelPlatform {
             }
         }
     }
+    
+    // void bubbleSortByHarga() {
+    //     for (int i = 0; i < daftarPenginapan.length - 1; i++) {
+    //         for (int j = 0; j < daftarPenginapan.length - 1 - i; j++) {
+    //             if (daftarPenginapan[j].harga > daftarPenginapan[j + 1].harga) {
+    //                 Penginapan temp = daftarPenginapan[j];
+    //                 daftarPenginapan[j] = daftarPenginapan[j + 1];
+    //                 daftarPenginapan[j + 1] = temp;
+    //             }
+    //         }
+    //     }
+    // }
 
     // Method Selection Sort untuk mengurutkan berdasarkan rating
     void selectionSortByRating() {
         for (int i = 0; i < daftarPenginapan.length - 1; i++) {
             int idxMax = i;
             for (int j = i + 1; j < daftarPenginapan.length; j++) {
-                if (daftarPenginapan[j].rating > daftarPenginapan[idxMax].rating) {
+                if (daftarPenginapan[j] != null && daftarPenginapan[idxMax] != null && 
+                    daftarPenginapan[j].rating > daftarPenginapan[idxMax].rating) {
                     idxMax = j;
                 }
             }
-            Penginapan temp = daftarPenginapan[idxMax];
-            daftarPenginapan[idxMax] = daftarPenginapan[i];
-            daftarPenginapan[i] = temp;
+            if (daftarPenginapan[idxMax] != null) {
+                Penginapan temp = daftarPenginapan[idxMax];
+                daftarPenginapan[idxMax] = daftarPenginapan[i];
+                daftarPenginapan[i] = temp;
+            }
         }
     }
+    
+    // void selectionSortByRating() {
+    //     for (int i = 0; i < daftarPenginapan.length - 1; i++) {
+    //         int idxMax = i;
+    //         for (int j = i + 1; j < daftarPenginapan.length; j++) {
+    //             if (daftarPenginapan[j].rating > daftarPenginapan[idxMax].rating) {
+    //                 idxMax = j;
+    //             }
+    //         }
+    //         Penginapan temp = daftarPenginapan[idxMax];
+    //         daftarPenginapan[idxMax] = daftarPenginapan[i];
+    //         daftarPenginapan[i] = temp;
+    //     }
+    // }
 }
